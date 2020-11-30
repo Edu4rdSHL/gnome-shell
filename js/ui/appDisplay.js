b215084f1a944dd1b43590fa2a2e88a89511add2
Aliya Rahmani <aliyarahmani786@gmail.com>
2020-11-30 06:51:35 +0000

    appDisplay: Fix removing empty folders
    https://gitlab.gnome.org/GNOME/gnome-shell/-/issues/3077

--- js/ui/appDisplay.js
+++ js/ui/appDisplay.js
@@ -1151,6 +1151,7 @@ class AppDisplay extends BaseAppView {
 
             // Don't try to display empty folders
             if (!icon.visible) {
+                this._removeItem(icon);
                 icon.destroy();
                 return;
             }
