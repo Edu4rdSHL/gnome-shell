2aaab0be3ecd6286989e1ff93d4d144f5663b9db
Monster <bgravendeel@proton.me>
2024-03-28 20:15:22 +0100

    screenshot: Better align messages with the HIG
    Move titles to title case, remove periods from subtitles, reword titles
    to better fit their role.

--- js/ui/screenshot.js
+++ js/ui/screenshot.js
@@ -2046,7 +2046,7 @@ export const ScreenshotUI = GObject.registerClass({
         }
 
         // Translators: notification title.
-        this._showNotification(_('Screencast recorded'));
+        this._showNotification(_('Screencast Recorded'));
     }
 
     _screencastFailed(phase, error) {
@@ -2059,22 +2059,22 @@ export const ScreenshotUI = GObject.registerClass({
             delete this._screencastPath;
 
             // Translators: notification title.
-            this._showNotification(_('Screencast failed to start'));
+            this._showNotification(_('Screencast Failed to Start'));
             break;
 
         case ScreencastPhase.RECORDING:
             if (error.matches(ScreencastErrors, ScreencastError.OUT_OF_DISK_SPACE)) {
                 // Translators: notification title.
-                this._showNotification(_('Screencast ended: Out of disk space'));
+                this._showNotification(_('Screencast Failed: Out of Disk Space'));
             } else if (error.matches(ScreencastErrors, ScreencastError.SERVICE_CRASH)) {
                 // We can encourage user to try again on service crashes since the
                 // recorder will auto-blocklist the pipeline that crashed.
 
                 // Translators: notification title.
-                this._showNotification(_('Screencast ended unexpectedly, please try again'));
+                this._showNotification(_('Screencast Failed, Please Try Again'));
             } else {
                 // Translators: notification title.
-                this._showNotification(_('Screencast ended unexpectedly'));
+                this._showNotification(_('Screencast Failed'));
             }
 
             break;
@@ -2091,7 +2091,7 @@ export const ScreenshotUI = GObject.registerClass({
             source,
             title,
             // Translators: notification body when a screencast was recorded.
-            body: this._screencastPath ? _('Click here to view the video.') : '',
+            body: this._screencastPath ? _('Click here to view the video') : '',
             isTransient: true,
         });
 
@@ -2332,9 +2332,9 @@ function _storeScreenshot(bytes, pixbuf) {
     const notification = new MessageTray.Notification({
         source,
         // Translators: notification title.
-        title: _('Screenshot captured'),
+        title: _('Screenshot Captured'),
         // Translators: notification body when a screenshot was captured.
-        body: _('You can paste the image from the clipboard.'),
+        body: _('You can paste the image from the clipboard'),
         datetime: time,
         gicon: content,
         isTransient: true,
