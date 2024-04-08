7acbce16f718f283329b0839b0fb167b63f9861f
slaclau <seb.laclau@gmail.com>
2024-04-08 19:06:42 +0100

    Another try for background apps

--- js/ui/status/backgroundApps.js
+++ js/ui/status/backgroundApps.js
@@ -45,6 +45,7 @@ const BackgroundAppMenuItem = GObject.registerClass({
 
         this.add_style_class_name('background-app-item');
         this.label.add_style_class_name('title');
+        this.label.y_expand = false;
 
         this.app = app;
 
