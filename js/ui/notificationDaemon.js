21fd4f1a1c7283ad3392b9fd64bbdc083ab912b3
Julian Sparber <julian@sparber.net>
2024-04-10 19:37:37 +0200

    fdoNotificationDaemon: Show banner for resident notification when app has focus
    This drops an undocumented behavior that was introduced for media
    players (they now use MPRIS anyways).

--- js/ui/notificationDaemon.js
+++ js/ui/notificationDaemon.js
@@ -357,12 +357,6 @@ class FdoNotificationDaemonSource extends MessageTray.Source {
             this.notify('icon');
         }
 
-        let tracker = Shell.WindowTracker.get_default();
-        // Acknowledge notifications that are resident and their app has the
-        // current focus so that we don't show a banner.
-        if (notification.resident && this.app && tracker.focus_app === this.app)
-            notification.acknowledged = true;
-
         this.addNotification(notification);
     }
 
