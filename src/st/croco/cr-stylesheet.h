lazerl0rd/gnome-shell-perf

croco/stylesheet: Define the autoptr cleanup function for CRStyleSheet

src/st/croco/cr-stylesheet.h
@@ -89,6 +89,8 @@ void cr_stylesheet_destroy (CRStyleSheet *a_this) ;
 void cr_stylesheet_set_app_data (CRStyleSheet *a_this, gpointer app_data, GDestroyNotify app_data_destroy_func);
 gpointer cr_stylesheet_get_app_data (CRStyleSheet *a_this);
 
+G_DEFINE_AUTOPTR_CLEANUP_FUNC (CRStyleSheet, cr_stylesheet_ref);
+
 G_END_DECLS
 
 #endif /*__CR_STYLESHEET_H__*/

Edu4rdSHL/gnome-shell

src/st/croco/cr-stylesheet.h
@@ -85,6 +85,8 @@ void cr_stylesheet_destroy (CRStyleSheet *a_this) ;
 void cr_stylesheet_set_app_data (CRStyleSheet *a_this, gpointer app_data, GDestroyNotify app_data_destroy_func);
 gpointer cr_stylesheet_get_app_data (CRStyleSheet *a_this);
 
+G_DEFINE_AUTOPTR_CLEANUP_FUNC (CRStyleSheet, cr_stylesheet_unref);
+
 G_END_DECLS
 
 #endif /*__CR_STYLESHEET_H__*/