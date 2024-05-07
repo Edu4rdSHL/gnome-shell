prototypefund/verdre-gnome-shell

unlockDialog: Move from TapAction to ClickGesture
ClutterClickGesture is a new subclass of ClutterGesture and implements
the same functionality as the TapAction and ClutterClickAction did,
which is why those will get removed. So move to ClutterClickGesture here
for the tap gesture.

js/ui/unlockDialog.js
@@ -514,9 +514,9 @@ var UnlockDialog = GObject.registerClass({
 
         this._activePage = null;
 
-        let tapAction = new Clutter.TapAction();
-        tapAction.connect('tap', this._showPrompt.bind(this));
-        this.add_action(tapAction);
+        const clickGesture = new Clutter.ClickGesture();
+        clickGesture.connect('clicked', () => this._showPrompt());
+        this.add_action(clickGesture);
 
         // Background
         this._backgroundGroup = new Clutter.Actor();

Edu4rdSHL/gnome-shell

js/ui/unlockDialog.js
@@ -524,9 +524,9 @@ export const UnlockDialog = GObject.registerClass({
 
         this._activePage = null;
 
-        let tapAction = new Clutter.TapAction();
-        tapAction.connect('tap', this._showPrompt.bind(this));
-        this.add_action(tapAction);
+        const clickGesture = new Clutter.ClickGesture();
+        clickGesture.connect('recognize', () => this._showPrompt());
+        this.add_action(clickGesture);
 
         // Background
         this._backgroundGroup = new Clutter.Actor();