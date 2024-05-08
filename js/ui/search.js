prototypefund/verdre-gnome-shell

Switch to ClutterPanGesture for recognizing pan gestures
ClutterPanGesture replaced ClutterPanAction and uses Clutters new
gesture tracker, the switch to the shiny new thing.

js/ui/search.js
@@ -563,9 +563,9 @@ var SearchResultsView = GObject.registerClass({
         this._scrollView.set_policy(St.PolicyType.NEVER, St.PolicyType.AUTOMATIC);
         this._scrollView.add_actor(this._content);
 
-        let action = new Clutter.PanAction({ interpolate: true });
-        action.connect('pan', this._onPan.bind(this));
-        this._scrollView.add_action(action);
+        const panGesture = new Clutter.PanGesture();
+        panGesture.connect('pan-update', this._onPanUpdate.bind(this));
+        this._scrollView.add_action(panGesture);
 
         this.add_child(this._scrollView);
 
@@ -735,11 +735,9 @@ var SearchResultsView = GObject.registerClass({
         this.emit('terms-changed');
     }
 
-    _onPan(action) {
-        let [dist_, dx_, dy] = action.get_motion_delta(0);
+    _onPanUpdate(action, deltaX, deltaY, totalDistance) {
         let adjustment = this._scrollView.vscroll.adjustment;
-        adjustment.value -= (dy / this.height) * adjustment.page_size;
-        return false;
+        adjustment.value -= (deltaY / this.height) * adjustment.page_size;
     }
 
     _focusChildChanged(provider) {

Edu4rdSHL/gnome-shell

js/ui/search.js
@@ -579,9 +579,9 @@ export const SearchResultsView = GObject.registerClass({
             child: this._content,
         });
 
-        let action = new Clutter.PanAction({interpolate: true});
-        action.connect('pan', this._onPan.bind(this));
-        this._scrollView.add_action(action);
+        const panGesture = new Clutter.PanGesture();
+        panGesture.connect('pan-update', this._onPanUpdate.bind(this));
+        this._scrollView.add_action(panGesture);
 
         this.add_child(this._scrollView);
 
@@ -751,11 +751,9 @@ export const SearchResultsView = GObject.registerClass({
         this.emit('terms-changed');
     }
 
-    _onPan(action) {
-        let [dist_, dx_, dy] = action.get_motion_delta(0);
+    _onPanUpdate(action, deltaX, deltaY, _totalDistance) {
         let adjustment = this._scrollView.vadjustment;
-        adjustment.value -= (dy / this.height) * adjustment.page_size;
-        return false;
+        adjustment.value -= (deltaY / this.height) * adjustment.page_size;
     }
 
     _focusChildChanged(provider) {